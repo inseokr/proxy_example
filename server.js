d07444f3ea22db621cdf06f302484645d892dd9a
In Seo <inseo.kr@gmail.com>
2020-10-26 21:15:59 -0700

    after fixing all the issues to get local env working

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 5000;
+//const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
